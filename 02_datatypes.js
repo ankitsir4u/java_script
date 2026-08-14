0676cf14d2d05734c12d7b2204bab6c29efc7d2c
ankitsir4u <ankitsir4u@gmail.com>
2026-08-14 23:08:10 +0530

    add 02_datatypes.js

---
+++ 02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, nott brower
+
+console.log(3 + 3)
+ console.log ("hitesh")
+
+ let name = "hitesh"
+ let age = 18
+ let isloggedIn = false
+
+// types of data  type
+ //number => 2 to the power of 53
+ // big int 
+// boolean => standalone value 
+// undefined => 
+    // symbol=> unique 
+
+// object
+
+console.log (typeof age)
