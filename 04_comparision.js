14a01607e37038144dc843e1ce0294215157cb75
ankitsir4u <ankitsir4u@gmail.com>
2026-08-16 17:06:10 +0530

    newly

---
+++ 04_comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log(null > 0);
+console.log(null == 0); // equality and comparision are different things
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined > 0);
+// in most  code we are avoid this tyoe of code .\
+
