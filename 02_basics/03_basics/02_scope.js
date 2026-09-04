54430045f51aa418a3179ad31228591f15bf14f0
ankitsir4u <ankitsir4u@gmail.com>
2026-09-04 22:40:47 +0530

    Added JavaScript files

---
+++ 02_basics/03_basics/02_scope.js
@@ -0,0 +1,55 @@
+let a = 300
+
+if (true) {
+    let a = 10
+    const  b = 20
+// console.log("INNER: ", a);
+function addnum(){
+}
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one()
+{
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+// console.log(website);
+two()
+
+}
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh"){
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+}
+
+// console.log(website);
+
+// ++++++++++++++++++++++ Intesresting +++++++++++++++++++
+coin = addone(5);
+console.log(coin);
+
+
+function addone(num){
+    return num + 1
+}
+
+
+// addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
